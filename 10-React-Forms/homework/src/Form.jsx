955d18465beff5428dd83ebe45baf2d7afcfc888
AleAndino <aleandino.cba@gmail.com>
2022-07-21 16:55:27 -0300

    HW FORMS

--- 10-React-Forms/homework/src/Form.jsx
+++ 10-React-Forms/homework/src/Form.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function  Form() {
   return (
+    <form>
       <div>
-        Componente Form
+        <label htmlFor="">Username:</label>
+        <input type="text" name='username' />
       </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" name='password' />
+      </div>
+      <div>
+        <input type="submit" name='submit'/>
+      </div>
+    </form>
   )
 }
